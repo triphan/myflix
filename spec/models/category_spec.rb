427d46809fdc0bc1c1f9834f2fa8c8009a7e91fc
Tri Phan <tphan414@gmail.com>
2015-07-26 21:04:20 -0700

    add search action

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,14 @@ require 'spec_helper'
 describe Category do
   it { should have_many(:videos)}
   it { should validate_presence_of(:name)}
+
+  describe "recent_videos" do
+    it "returns an array of 6 videos" do
+      category = Category.create(name: "category")
+      6.times { Video.create(title: "title", description: "Desc", category: category) }
+      expect(category.recent_videos.count).to eq(6)
+    end
+    it "returns an array of 6 videos with created at DESC"
+
+  end
 end
